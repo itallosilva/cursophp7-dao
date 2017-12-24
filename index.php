5a612cbd52311243a2ee801d41471a9d7de4948e
Itallo Silva <itallosilva@yahoo.com.br>
2017-12-24 14:18:07 -0300

    Métodos de lista

--- index.php
+++ index.php
@@ -2,15 +2,36 @@
 
 	require_once("config.php");
 
-	$root = new Usuario();
-	$root->loadById(1);
-
-	echo $root;
-
-/*	$sql = new Sql();
+/*	
+	$sql = new Sql();
 
 	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 	echo json_encode($usuarios);
-*/
+
+
+	//Carrega um usuário
+		//$root = new Usuario();
+		//$root->loadById(1);
+		//echo $root;
+	*/
+	
+	//Carrega uma lista de usuários
+		//$lista = Usuario::getList();
+
+		//echo json_encode($lista);
+
+	//Carrega uma lista de usuários buscando pelo login
+		//$search = Usuario::search("jo");
+		//echo json_encode($search);
+
+	//Carrega um usuário usando o login e a senha
+		$usuario = new Usuario();
+
+		$usuario->login('joao','123456');
+
+		echo ($usuario);
+
+
+
 ?>
